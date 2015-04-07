298616102cbc51914850c176b42118acf37276d0
Visse <visse44@gmail.com>
2015-04-07 08:21:47 +0200

    Added alpha uniform for DebugDrawerTexture

--- resources/core/shaders/DebugDrawerTexture.glsl
+++ resources/core/shaders/DebugDrawerTexture.glsl
@@ -6,7 +6,13 @@ out vec4 color;
 
 uniform sampler2D Texture;
 
+uniform Uniforms {
+    vec4 quad;
+    float alpha;
+};
+
 void main()
 {
     color = texture2D(Texture,texcoord);
+    color.a = alpha;
 }
